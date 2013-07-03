8dd4f40019281b52e6a5a9038067e3e6f63d05e9
jacklin1011 <jacklin1011@163.com>
2013-07-03 13:44:09 +0800

    Initial Commit

---
+++ TileGame/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  TileGame
+//
+//  Created by 林光海 on 13-7-3.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ TileGame/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  TileGame
+//
+//  Created by 林光海 on 13-7-3.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
